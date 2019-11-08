19687db49f03f815c20b44fb5d83116466497cf5
Gustavo Gois <gustavo.gois@gmail.com>
2019-11-08 23:55:58 +0000

    Initial Commit

---
+++ src/main/java/gois/study/sbobkjava8inaction/SboBkJava8InActionApplication.java
@@ -0,0 +1,13 @@
+package gois.study.sbobkjava8inaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SboBkJava8InActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SboBkJava8InActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gois/study/sbobkjava8inaction/SboBkJava8InActionApplicationTests.java
@@ -0,0 +1,13 @@
+package gois.study.sbobkjava8inaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SboBkJava8InActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
