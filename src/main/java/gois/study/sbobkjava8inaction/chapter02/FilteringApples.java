259c1e9d02909e8956d8cc05643f04cc326282a2
Gustavo Gois <gustavo.gois@gmail.com>
2019-11-10 08:25:39 +0000

    Implementation (green) - closes #2

--- src/main/java/gois/study/sbobkjava8inaction/chapter02/FilteringApples.java
+++ src/main/java/gois/study/sbobkjava8inaction/chapter02/FilteringApples.java
@@ -2,12 +2,19 @@ package gois.study.sbobkjava8inaction.chapter02;
 
 import gois.study.sbobkjava8inaction.model.Apple;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class FilteringApples {
 
-    public static List<Apple> filter(List<Apple> apples, Predicate predicate) {
-        return null;
+    public static List<Apple> filter(List<Apple> apples, Predicate<Apple> predicate) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : apples) {
+            if (predicate.test(apple)) {
+                result.add(apple);
+            }
+        }
+        return result;
     }
 }
