ffc9c6f764e9bb0c18a27810105173ae4d42acd4
Gustavo Gois <gustavo.gois@gmail.com>
2019-11-12 23:30:49 +0000

    Closes #8

--- src/main/java/gois/study/sbobkjava8inaction/chapter03/ExecuteAroundPattern.java
+++ src/main/java/gois/study/sbobkjava8inaction/chapter03/ExecuteAroundPattern.java
@@ -14,4 +14,14 @@ public class ExecuteAroundPattern {
         return "";
     }
 
+    public static String processWithLambda(BufferedReaderProcessor bufferedReaderProcessor) {
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/data.txt"))) {
+            return bufferedReaderProcessor.process(br);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
--- src/test/java/gois/study/sbobkjava8inaction/chapter03/ExecuteAroundPatternTest.java
+++ src/test/java/gois/study/sbobkjava8inaction/chapter03/ExecuteAroundPatternTest.java
@@ -2,6 +2,8 @@ package gois.study.sbobkjava8inaction.chapter03;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,5 +14,12 @@ class ExecuteAroundPatternTest {
         String result = ExecuteAroundPattern.process();
 
         assertThat(result).isEqualTo("Linha 01");
+    }
+
+    @Test
+    void processWithLambda() {
+        String result = ExecuteAroundPattern.processWithLambda((BufferedReader br) -> br.readLine() + br.readLine());
+
+        assertThat(result).isEqualTo("Linha 01Linha 02");
     }
 }
