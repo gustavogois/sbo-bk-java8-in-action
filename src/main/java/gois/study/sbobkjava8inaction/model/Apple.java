a2c49522caa5bc9e7761fe7c2172daa8814b8c1a
Gustavo Gois <gustavo.gois@gmail.com>
2019-11-15 00:02:37 +0000

    Closes #14

--- src/main/java/gois/study/sbobkjava8inaction/model/Apple.java
+++ src/main/java/gois/study/sbobkjava8inaction/model/Apple.java
@@ -1,35 +1,18 @@
 package gois.study.sbobkjava8inaction.model;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Apple {
 
     private int weight = 0;
     private String color = "";
 
-    public Apple(int weight, String color){
-        this.weight = weight;
-        this.color = color;
-    }
-
     public Integer getWeight() {
         return weight;
-    }
-
-    public void setWeight(Integer weight) {
-        this.weight = weight;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String toString() {
-        return "Apple{" +
-                "color='" + color + '\'' +
-                ", weight=" + weight +
-                '}';
     }
 }
