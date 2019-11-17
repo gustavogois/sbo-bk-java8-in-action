e3f881f8f6badf88687098d304459000938acd96
Gustavo Gois <gustavo.gois@gmail.com>
2019-11-17 10:36:28 +0000

    closes #28

--- src/main/java/gois/study/sbobkjava8inaction/chapter05/FindingAndMatching.java
+++ src/main/java/gois/study/sbobkjava8inaction/chapter05/FindingAndMatching.java
@@ -3,6 +3,7 @@ package gois.study.sbobkjava8inaction.chapter05;
 import gois.study.sbobkjava8inaction.model.Dish;
 
 import java.util.List;
+import java.util.Optional;
 
 public class FindingAndMatching {
     public static boolean hasVegetarian(List<Dish> menu) {
@@ -15,5 +16,17 @@ public class FindingAndMatching {
         //        .allMatch(dish -> dish.getCalories() < 1000);
         return menu.stream()
                 .noneMatch(dish -> dish.getCalories() >= 1000);
+    }
+
+    public static Optional<Dish> findAVegetarianDish(List<Dish> menu) {
+        /*return menu.stream()
+                .filter(Dish::isVegetarian)
+                .findFirst();*/
+        return menu.stream()
+                .filter(Dish::isVegetarian)
+                .findAny();
+        /*Finding the first element is more constraining in parallel. If you don’t care about which element is
+            returned, use findAny because it’s less constraining when using parallel streams.
+         */
     }
 }
--- src/test/java/gois/study/sbobkjava8inaction/chapter05/FindingAndMatchingTest.java
+++ src/test/java/gois/study/sbobkjava8inaction/chapter05/FindingAndMatchingTest.java
@@ -3,6 +3,8 @@ package gois.study.sbobkjava8inaction.chapter05;
 import gois.study.sbobkjava8inaction.model.Dish;
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FindingAndMatchingTest {
@@ -19,5 +21,12 @@ public class FindingAndMatchingTest {
         boolean result = FindingAndMatching.isHealthy(Dish.getMenu());
 
         assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    public void findAVegetarianDish() {
+        Optional<Dish> aVegetarianDish = FindingAndMatching.findAVegetarianDish(Dish.getMenu());
+
+        assertThat(aVegetarianDish.isPresent()).isEqualTo(true);
     }
 }
