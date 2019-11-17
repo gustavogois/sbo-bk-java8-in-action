54615ac6d2d14cbf96bf8c5ddf1b081a572bd060
Gustavo Gois <gustavo.gois@gmail.com>
2019-11-17 10:50:03 +0000

    relates #29

--- src/main/java/gois/study/sbobkjava8inaction/chapter05/Reducing.java
+++ src/main/java/gois/study/sbobkjava8inaction/chapter05/Reducing.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Reducing {
     public static int sumAllElements(List<Integer> list) {
         return list.stream()
-                .reduce(0, (a, b) -> a + b);
+                //.reduce(0, (a, b) -> a + b);
+                .reduce(0, Integer::sum);
     }
 }
